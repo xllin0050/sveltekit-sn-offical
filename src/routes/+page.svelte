96ee6e8dd17a7debd7d9ad8cccd2ee29a261028f
RD-Carl <carl@local.tc>
2023-03-06 15:28:19 +0800

    add: member circle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	// get supabase data at server side
 	import type { PageData } from './$types';
+	import MembersNameCircle from './MembersNameCircle.svelte';
 	export let data: PageData;
 	const { nextGig } = data;
 </script>
 
 <section class="max-w-screen-lg mx-auto">
-home
+	<MembersNameCircle />
 </section>
---
+++ src/routes/MembersNameCircle.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	let shihDrum: HTMLElement;
+	let yauGtr: HTMLElement;
+	let weitingBass: HTMLElement;
+	onMount(() => {
+		const nameCircle = (nameText: HTMLElement, position: number) => {
+			nameText.innerHTML = nameText.innerText
+				.split('')
+				.map(
+					(char: string, i: number) =>
+						`<span style="transform:rotate(${(i + position) * 8}deg)">${char}</span>`
+				)
+				.join('');
+		};
+		nameCircle(shihDrum, 15);
+		nameCircle(yauGtr, 0);
+		nameCircle(weitingBass, 28);
+	});
+
+	const membersName = ['shih', 'yau', 'weiting'];
+	let nameIndex = 0;
+	let backgroundName = 'yau';
+	const changeNameTimer = setInterval(() => {
+		nameIndex++;
+		if (nameIndex === 3) nameIndex = 0;
+		backgroundName = membersName[nameIndex];
+	}, 1600);
+	onDestroy(() => {
+		clearInterval(changeNameTimer);
+	});
+</script>
+
+<div class="pointer-events-none relative my-0 h-[380px] w-full pb-0 lg:h-[775px]">
+	<div class="hidden w-full lg:flex lg:items-center lg:justify-center">
+		<div
+			class="relative block h-[550px] w-[550px] translate-y-28 rounded-full font-redhat text-xl font-medium"
+		>
+			<p
+				bind:this={shihDrum}
+				class="nameText"
+				class:text-neutral-700={backgroundName === 'shih'}
+				class:text-neutral-200={backgroundName !== 'shih'}
+			>
+				shih(drum)
+			</p>
+			<p
+				bind:this={yauGtr}
+				class="nameText"
+				class:text-neutral-700={backgroundName === 'yau'}
+				class:text-neutral-200={backgroundName !== 'yau'}
+			>
+				yau(gtr/vox)
+			</p>
+			<p
+				bind:this={weitingBass}
+				class="nameText"
+				class:text-neutral-700={backgroundName === 'weiting'}
+				class:text-neutral-200={backgroundName !== 'weiting'}
+			>
+				weiting(bass)
+			</p>
+		</div>
+	</div>
+	{#each membersName as name}
+		<div
+			class="absolute top-1/2 left-1/2 h-60 w-60 -translate-x-1/2 -translate-y-1/2 rounded-full shadow-lg lg:h-[380px] lg:w-[380px] {name}"
+			class:opacity-100={name === backgroundName}
+			class:opacity-0={name !== backgroundName}
+		>
+			<div
+				class="absolute bottom-4 w-full text-center uppercase tracking-widest text-neutral-50 opacity-100 lg:opacity-0"
+			>
+				{name}
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	@keyframes rotateText {
+		0% {
+			transform: rotate(360deg);
+		}
+	}
+	.nameText {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		text-transform: uppercase;
+		animation: rotateText 24s linear infinite;
+	}
+	:global(.nameText > span) {
+		position: absolute;
+		left: 50%;
+		transform-origin: 0 275px;
+	}
+	.shih {
+		background: url('$lib/assets/images/members/shih.jpg');
+		background-size: cover;
+		background-position: center;
+	}
+	.yau {
+		background: url('$lib/assets/images/members/yau.jpg');
+		background-size: cover;
+		background-position: left;
+	}
+	.weiting {
+		background: url('$lib/assets/images/members/weiting.jpg');
+		background-size: cover;
+		background-position: left;
+	}
+</style>
